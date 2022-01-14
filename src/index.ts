da23e279d2d4c6c587dcc06595e5c5442e1f88de
meizi wen <meizi.wen@supernovacompanies.biz>
2022-01-14 17:57:20 +0800

    log

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,19 @@
 class Logger {
     private log(...data: any) {
-      console.log(...data);
+        console.log(...data);
     }
-    info(
-      options: any = {
-        color: 'black',
-        textShadow: '',
-        fontFamily: '',
-        fontSize: '14px',
-        padding: '',
-      },
-      ...msg: any
-    ) {
-      const styles = [
-        `color: ${options.color}`,
-        `font-size: ${options.fontSize}`,
-        `font-family: ${options.fontFamily}`,
-        `text-shadow: ${options.textShadow}`,
-        `padding: ${options.padding}`,
-      ].join(';');
-      this.log(...msg, styles);
+    info(options?: any, ...msg: any) {
+        const styles = [
+            `color: ${options.color || ''}`,
+            `font-size: ${options.fontSize || ''}`,
+            `font-family: ${options.fontFamily || ''}`,
+            `text-shadow: ${options.textShadow || ''}`,
+            `padding: ${options.padding || ''}`,
+        ].join(';');
+        this.log(JSON.stringify(msg), styles);
     }
     success(...msg: any) {
-      this.log({color: 'red'}, `%c${JSON.stringify(msg)}`);
+        this.log({ color: 'red' }, `%c${JSON.stringify(msg)}`);
     }
-  }
-  export default new Logger();
-  
+}
+export default new Logger();
