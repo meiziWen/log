f7045021d38756d4bff9422844de0f2d27fc3a34
meizi wen <meizi.wen@supernovacompanies.biz>
2022-01-17 13:59:09 +0800

    v1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,33 @@
 declare class Logger {
+    /**
+     *
+     * enum css property
+     */
+    private customStyle;
     private log;
-    info(output: string): void;
+    custom(options: any, msg: string): void;
+    /**
+     *
+     * custom
+     */
+    info(options?: any, ...msg: any): void;
+    success(...msg: any): void;
+    /**
+     *
+     * High contrast, white text and green background color, indicate successful msg.
+     */
+    SUCCESS(...msg: any): void;
+    error(...msg: any): void;
+    /**
+     *
+     * High contrast, white text and red background color, indicate fail msg.
+     */
+    ERROR(...msg: any): void;
+    blue(...msg: any): void;
+    red(...msg: any): void;
+    BLUE(...msg: any): void;
+    RED(...msg: any): void;
+    BLACK(...msg: any): void;
 }
-declare const log: Logger;
-export default log;
+declare const _default: Logger;
+export default _default;
--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,141 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Logger {
-    log(output) {
-        console.log(output);
+var Logger = /** @class */ (function () {
+    function Logger() {
+        /**
+         *
+         * enum css property
+         */
+        this.customStyle = {
+            color: '',
+            bg: '',
+            fontFamily: '',
+            textShadow: '',
+            padding: '5px 10px',
+            fontSize: '18px',
+        };
     }
-    info(output) {
-        this.log(output);
-    }
-}
-const log = new Logger();
-exports.default = log;
+    Logger.prototype.log = function () {
+        var data = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            data[_i] = arguments[_i];
+        }
+        console.log.apply(console, data);
+    };
+    Logger.prototype.custom = function (options, msg) {
+        if (options === void 0) { options = {}; }
+        if (JSON.stringify(options) === '{}')
+            return this.log(msg);
+        var styles = [];
+        // 因为部分属性会用缩写, 且console并不支持所有样式，所以此处枚举
+        if (options.color)
+            styles.push("color: ".concat(options.color));
+        if (options.fontSize)
+            styles.push("font-size: ".concat(options.fontSize));
+        if (options.fontFamily)
+            styles.push("font-family: ".concat(options.fontFamily));
+        if (options.textShadow)
+            styles.push("text-shadow: ".concat(options.textShadow));
+        if (options.padding)
+            styles.push("padding: ".concat(options.padding));
+        if (options.bg)
+            styles.push("background-color: ".concat(options.bg));
+        var styleStr = styles.join(';');
+        this.log("%c".concat(JSON.stringify(msg).substring(1, JSON.stringify(msg).length - 1)), styleStr);
+    };
+    /**
+     *
+     * custom
+     */
+    Logger.prototype.info = function (options) {
+        if (options === void 0) { options = {}; }
+        var msg = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            msg[_i - 1] = arguments[_i];
+        }
+        this.custom(options, msg);
+    };
+    Logger.prototype.success = function () {
+        var msg = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            msg[_i] = arguments[_i];
+        }
+        this.custom({ color: 'green' }, msg);
+    };
+    /**
+     *
+     * High contrast, white text and green background color, indicate successful msg.
+     */
+    Logger.prototype.SUCCESS = function () {
+        var msg = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            msg[_i] = arguments[_i];
+        }
+        this.customStyle.color = 'white';
+        this.customStyle.bg = 'green';
+        this.custom(this.customStyle, msg);
+    };
+    Logger.prototype.error = function () {
+        var msg = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            msg[_i] = arguments[_i];
+        }
+        this.custom({ color: 'red' }, msg);
+    };
+    /**
+     *
+     * High contrast, white text and red background color, indicate fail msg.
+     */
+    Logger.prototype.ERROR = function () {
+        var msg = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            msg[_i] = arguments[_i];
+        }
+        this.customStyle.color = 'white';
+        this.customStyle.bg = 'red';
+        this.custom(this.customStyle, msg);
+    };
+    Logger.prototype.blue = function () {
+        var msg = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            msg[_i] = arguments[_i];
+        }
+        this.custom({ color: 'blue' }, msg);
+    };
+    Logger.prototype.red = function () {
+        var msg = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            msg[_i] = arguments[_i];
+        }
+        this.custom({ color: 'red' }, msg);
+    };
+    Logger.prototype.BLUE = function () {
+        var msg = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            msg[_i] = arguments[_i];
+        }
+        this.customStyle.color = 'white';
+        this.customStyle.bg = 'blue';
+        this.custom(this.customStyle, msg);
+    };
+    Logger.prototype.RED = function () {
+        var msg = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            msg[_i] = arguments[_i];
+        }
+        this.customStyle.color = 'white';
+        this.customStyle.bg = 'red';
+        this.custom(this.customStyle, msg);
+    };
+    Logger.prototype.BLACK = function () {
+        var msg = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            msg[_i] = arguments[_i];
+        }
+        this.customStyle.color = 'gold';
+        this.customStyle.bg = 'black';
+        this.custom(this.customStyle, msg);
+    };
+    return Logger;
+}());
+exports.default = new Logger();
